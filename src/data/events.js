8816235f33789ca04a537da3df6c9ec1998a5584
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-07-04 07:19:11 +0530

    added Technovista 2k25 details, image, update event navigation.

--- src/data/events.js
+++ src/data/events.js
@@ -36,7 +36,27 @@
 //   | 'Guest Lecture';
 
 const events = {
-  upcoming: [],
+  upcoming: [
+    {
+      name: "Technovista 2k25",
+      eventId: "tv25",
+      image: "/events/Technovista2025/img1.png",
+      date: "July 31st, August 1st & 2nd 2025",
+      venue: "VNR VJIET",
+      timings: "9:30 AM to 4:00 PM",
+      pics: [
+        "/events/Technovista2025/main.png",
+      ],
+      // winners:"",
+      // outcome:"",
+      // register: "/register",
+      isGFormEmbeddable: false,
+      event_tags: ["Tech Fest", "Hackathon", "Workshops"],
+      description:
+        "Join the electrifying second edition of Technovista 2K25 by VJ DataQuesters! A 3-day tech carnival packed with Hackathons, Debug Battles, Workshops, and Tech Talks — where innovation meets creativity.",
+      link: "https://tv-demo-eight.vercel.app/",
+    },
+  ],
   past: {
     e2025: [
       {
