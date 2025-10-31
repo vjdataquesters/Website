71e2d38b3f9bcdf2192baa7a86e2f1e571e1bff3
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-10-31 23:42:58 +0530

    red path

--- src/pages/Hit.jsx
+++ src/pages/Hit.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import data from "../data/hit.json";
+import data from "../data/hitData/hit.json";
 import bg_img from "../assets/bg_img.jpg";
 
 /**
