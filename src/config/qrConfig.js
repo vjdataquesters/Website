c2a7fab3e572b6a30307b19bcc01b870ed94d16e
JagadeeswarJ <jagadeeswar079@gmail.com>
2026-03-11 09:24:19 +0530

    .

--- src/config/qrConfig.js
+++ src/config/qrConfig.js
@@ -2,7 +2,7 @@ const USE_VNR = true;
 
 export const QR_CONFIG = {
   vnr: { qrImage: "/qr/vnr-qr.jpg", qrValue: "vnr" },
-  dq:  { qrImage: "/qr/dq-qr.jpg",  qrValue: "dq"  },
+  dq:  { qrImage: "/qr/dq-qr.jpg",  qrValue: "vjdq"  },
 };
 
-export const activeQR = USE_VNR ? "vnr" : "dq";
+export const activeQR = USE_VNR ? "vnr" : "vjdq";
